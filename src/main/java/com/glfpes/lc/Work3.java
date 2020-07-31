265b26d73bcd59eabcbbfebd2625c798e146fabf
杨光-Jeff <yangguang03@qiyi.com>
2020-07-31 11:07:01 +0800

    add work3 brutal

--- src/main/java/com/glfpes/lc/Work3.java
+++ src/main/java/com/glfpes/lc/Work3.java
@@ -1,16 +1,31 @@
 package com.glfpes.lc;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Work3 {
     public static void main(String[] args) {
-
+        String testString = "au";
+        System.out.println(lengthOfLongestSubstring(testString));
     }
+
+    // brutal
     public static int lengthOfLongestSubstring(String s) {
+        if (s == null) {
+            return 0;
+        }
         int result = 0;
         for(int start = 0; start < s.length(); ++start) {
+            Set<Character> set = new HashSet<>();
             for (int end = start; end < s.length(); ++end) {
-                String substring = s.substring(start, end);
-
+                if (set.contains(s.charAt(end))) {
+                    break;
+                } else {
+                    set.add(s.charAt(end));
+                }
+                result = Math.max(result, set.size());
             }
         }
+        return result;
     }
 }
